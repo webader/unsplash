419b54d875902a0e15ddf6f0c80d31a4e06bc12f
webader <f,eirf-2>
2022-02-12 12:41:52 +0200

    fix: unsplash image

--- js/script.js
+++ js/script.js
@@ -1,35 +1,43 @@
+// поиск по кнопке
 document.querySelector("#input").addEventListener("keydown", (event) => {
-    if (event.key == "Enter")
-      apiRequest();
-  });
+if (event.key == "Enter") 
+  document.querySelector("#grid").textContent = ""; 
+  const url =  'https://api.unsplash.com/search/photos?query='+input.value+'&per_page=30&client_id=_foWWWtsZw2GIM9MQyzNKldN738LsKGoWCAkRn3ldfk'    // уть + ключ арі
+  fetch(url)  
+  .then(response => {
+    if (!response.ok) throw Error(response.statusText);
+      return response.json();
+    })  
+    .then(data => {
+      loadImages(data);
+    })  
+    .catch(error => console.log(error));   // если в ответе ошибка
+});
   
-  document.querySelector("#search").addEventListener("click", () => {
-      apiRequest();
-  });
-  
-  apiRequest = () => {  
-    document.querySelector("#grid").textContent = "";      
-    fetch('https://api.unsplash.com/search/photos?query='+input.value+'&per_page=30&client_id=_foWWWtsZw2GIM9MQyzNKldN738LsKGoWCAkRn3ldfk') //ключ арі
-  
-    .then(response => {
-      if (!response.ok) throw Error(response.statusText);
-        return response.json();
-     })  
-     .then(data => {
-        loadImages(data);
-     })  
-     .catch(error => console.log(error));   // если в ответе ошибка
+  // поиск по клику
+document.querySelector("#search").addEventListener("click", () => {
+  document.querySelector("#grid").textContent = ""; 
+  const url =  'https://api.unsplash.com/search/photos?query='+input.value+'&per_page=30&client_id=_foWWWtsZw2GIM9MQyzNKldN738LsKGoWCAkRn3ldfk'    // уть + ключ арі
+  fetch(url)  
+  .then(response => {
+    if (!response.ok) throw Error(response.statusText);
+      return response.json();
+    })  
+    .then(data => {
+      loadImages(data);
+    })  
+    .catch(error => console.log(error));   // если в ответе ошибка
+});
+ 
+loadImages = (data) => {
+  for(let i = 0;i < data.results.length;i++){
+    let image = document.createElement("div");
+    image.className = "img";
+    image.style.backgroundImage = "url("+data.results[i].urls.raw + "&w=1366&h=768" +")";
+    image.addEventListener("dblclick", function(){
+      window.open(data.results[i].links.download, '_blank');
+    })
+    document.querySelector("#grid").appendChild(image);
   }
-  
-  loadImages = (data) => {
-    for(let i = 0;i < data.results.length;i++){
-      let image = document.createElement("div");
-      image.className = "img";
-      image.style.backgroundImage = "url("+data.results[i].urls.raw + "&w=1366&h=768" +")";
-      image.addEventListener("dblclick", function(){
-        window.open(data.results[i].links.download, '_blank');
-      })
-      document.querySelector("#grid").appendChild(image);
-    }
-  }
+}
   
